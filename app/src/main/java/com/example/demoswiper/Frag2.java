6d435ce754c42a388ba03554b153ecb35ed6a098
17045697 <17045697@myrp.edu.sg>
2019-07-18 10:51:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demoswiper/Frag2.java
@@ -0,0 +1,27 @@
+package com.example.demoswiper;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag2 extends Fragment {
+    TextView tvFrag2;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_frag2, container, false);
+        tvFrag2 = view.findViewById(R.id.tvFrag2);
+
+        return view;
+    }
+
+}
